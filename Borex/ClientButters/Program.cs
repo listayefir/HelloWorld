df17be150d37d08a360f39d4c85a3a5c0af8fa24
Butters <butters@mail.ru>
2016-06-13 18:06:37 +0300

    Добавлен клиент Баттерса, поправлен класс аккаунт

---
+++ Borex/ClientButters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Borex;
+
+namespace ClientButters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+            account[Currencies.USD] = 100;
+            var server = new BorexServer();
+            server.Exchange(account, Currencies.USD, Currencies.EUR, 50);
+            foreach (Currencies e in Enum.GetValues(typeof(Currencies)))
+                Console.WriteLine("{0,-7}{1:0.00}", e, account[e]);
+            Console.ReadKey();
+
+        }
+    }
+}
